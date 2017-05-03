37add9333c69aa3cf0e06c5fa48500a2d37b392d
Eric Vandenberg <eric@vandevprod.com>
2017-05-02 21:31:04 -0700

    handle form submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { TextField } from 'material-ui';
+import { TextField, RaisedButton } from 'material-ui';
 
 class PostNew extends Component {
 
@@ -19,7 +19,7 @@ class PostNew extends Component {
     if (values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
       errors.email = 'Invalid email address';
     }
-    return errors
+    return errors;
   }
 
   renderTextField = ({ input, label, meta: { touched, error }, ...custom }) => (
@@ -31,9 +31,15 @@ class PostNew extends Component {
     />
   );
 
+  onSubmit = (values) => {
+    console.log(values);
+  };
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <div>
             <Field name="title" component={this.renderTextField} label="Title" />
           </div>
@@ -45,6 +51,9 @@ class PostNew extends Component {
           </div>
           <div>
             <Field name="email" component={this.renderTextField} label="Email" />
+          </div>
+          <div>
+            <RaisedButton type="submit" label="Primary" primary={true} style={{margin: '30px auto'}} />
           </div>
         </form>
     )
